19b71c003338ba250623e27ccaef2dd391e5cd64
a7medabdo6 <57114105+a7medabdo6@users.noreply.github.com>
2023-06-10 19:11:35 +0300

    Update Contact.js

--- src/Apis/Contact.js
+++ src/Apis/Contact.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 async function Postcontactus(Data) {
   try {
-    const response = await axios.post("http://167.71.53.196/contactus/", Data);
+    const response = await axios.post("http://46.101.241.139/contactus/", Data);
     console.log(response.data);
     return response.data;
   } catch (error) {
